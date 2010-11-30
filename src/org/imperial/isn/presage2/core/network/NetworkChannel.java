19ef6ca8a924c052725716487900f3bca38dba8e
Sam Macbeth <sm1106@imperial.ac.uk>
2010-11-30 13:35:34 +0000

    NetworkChannel interface, very basic message delivery only.

---
+++ src/org/imperial/isn/presage2/core/network/NetworkChannel.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.imperial.isn.presage2.core.network;
+
+/**
+ * <p>This interface represents a channel over which communication
+ * can take place in the network.</p>
+ * 
+ * @author Sam Macbeth
+ *
+ */
+public interface NetworkChannel {
+
+	/**
+	 * Deliver a message through the network channel.
+	 * @param m
+	 * @throws NetworkException
+	 */
+	public void deliverMessage(Message m) throws NetworkException;
+	
+	
+}
