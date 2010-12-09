69e239c2b8e4f00a78f13c9df0ac9cf32f50f9db
Sam Macbeth <sm1106@imperial.ac.uk>
2010-12-09 14:42:49 +0000

    Renamed NetworkControllerTest to follow test naming conventions.

--- test/org/imperial/isn/presage2/core/network/NetworkControllerTest.java
+++ test/org/imperial/isn/presage2/core/network/NetworkControllerTest.java
@@ -15,7 +15,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestNetworkController {
+public class NetworkControllerTest {
 
 	//NetworkController testController;
 	
