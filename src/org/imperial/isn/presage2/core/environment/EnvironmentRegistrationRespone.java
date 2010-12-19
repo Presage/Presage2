acfb1fb37aea5e31c11cf9f8239a416f90c7e5d0
Sam Macbeth <sam@mary-macbeths-imac-2.local>
2010-12-19 15:49:15 +0000

    EnvironmentConnector interface + base versions of classes for making environment registration requests and actions to perform on the environment.
    Signed-off-by: Sam Macbeth <sammacbeth@gmail.com>

---
+++ src/org/imperial/isn/presage2/core/environment/EnvironmentRegistrationRespone.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.imperial.isn.presage2.core.environment;
+
+import java.util.UUID;
+
+/**
+ * @author Sam Macbeth
+ *
+ */
+public class EnvironmentRegistrationRespone {
+
+	private UUID authKey;
+}
