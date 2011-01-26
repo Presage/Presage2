343279642b6e6636bf4b42a2995d49b14158a83b
Sam Macbeth <sm1106@imperial.ac.uk>
2011-01-26 16:45:50 +0000

    Basic environment exceptions.
    - InvalidAuthkeyException - for when an agent attempts to act with an incorrect authkey.
    - SharedStateAccessException - when an attempt is made to access non-existant shared state.

---
+++ src/uk/ac/imperial/presage2/core/environment/InvalidAuthkeyException.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package uk.ac.imperial.presage2.core.environment;
+
+/**
+ * @author Sam Macbeth
+ *
+ */
+public class InvalidAuthkeyException extends RuntimeException {
+
+	public InvalidAuthkeyException(String string) {
+		super(string);
+	}
+
+	/**
+	 * 
+	 */
+	public InvalidAuthkeyException() {
+		super();
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public InvalidAuthkeyException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public InvalidAuthkeyException(Throwable cause) {
+		super(cause);
+	}
+	
+	
+	
+}
