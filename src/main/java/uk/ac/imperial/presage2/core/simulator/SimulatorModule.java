ef7d4b0ea56824e44520c9fcdffa7adcd0dfd1a9
Sam Macbeth <sm1106@imperial.ac.uk>
2011-03-31 15:50:17 +0100

    Removing core's dependancy on JDO and added a hook to enable JDO support when the new presage2-db is on the classpath.

--- src/main/java/uk/ac/imperial/presage2/core/simulator/SimulatorModule.java
+++ src/main/java/uk/ac/imperial/presage2/core/simulator/SimulatorModule.java
@@ -3,26 +3,13 @@
  */
 package uk.ac.imperial.presage2.core.simulator;
 
-import java.io.File;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
-
-import javax.jdo.JDOHelper;
-import javax.jdo.PersistenceManager;
-import javax.jdo.PersistenceManagerFactory;
 
 import org.apache.log4j.Logger;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Inject;
-import com.google.inject.Provider;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
-import com.google.inject.name.Named;
-import com.google.inject.name.Names;
+import com.google.inject.Module;
 
 /**
  * <p>Global simulator level guice bindings</p>
@@ -37,15 +24,17 @@ public class SimulatorModule extends AbstractModule {
 	
 	@Override
 	protected void configure() {
-		// get properties for database binding
-		/*try {
-			Properties props = loadProperties();
-			Names.bindProperties(binder(), props);
-		} catch (Exception e) {
-			 logger.fatal("Unable to retrieve simulation config properties from file config.properties", e);
-		}*/
-		
-		
+		try {
+			Module dbModule = (Module) Class.forName("uk.ac.imperial.presage2.db.jdo.JDOModule").newInstance();
+			logger.info("Found JDO storage module, loading JDO support.");
+			install(dbModule);
+		} catch(ClassNotFoundException e) {
+			logger.info("No storage module found, running without db support.");
+		} catch (InstantiationException e) {
+			logger.error("Error loading JDO storage module.", e);
+		} catch (IllegalAccessException e) {
+			logger.error("Error loading JDO storage module.", e);
+		}
 	}
 	
 	/**
@@ -59,28 +48,6 @@ public class SimulatorModule extends AbstractModule {
 		 URL url = loader.getResource("config.properties");
 		 properties.load(url.openStream());
 		 return properties;
-	}
-	
-	@Provides @JDO
-	protected Properties bindJDOProperties() {
-		Properties p = new Properties();
-		p.setProperty("javax.jdo.PersistenceManagerFactoryClass", "org.datanucleus.api.jdo.JDOPersistenceManagerFactory");
-		p.setProperty("javax.jdo.option.ConnectionURL",  "");
-		p.setProperty("javax.jdo.option.ConnectionDriverName", "");
-		p.setProperty("javax.jdo.option.ConnectionUserName", "");
-		p.setProperty("javax.jdo.option.ConnectionPassword", "");
-		return p;
-	}
-	
-	@Provides @Singleton
-	PersistenceManagerFactory providePersistenceManagerFactory(@JDO Properties jdoProps) {
-		return JDOHelper.getPersistenceManagerFactory(jdoProps);
-	}
-	
-	@Provides
-	PersistenceManager providePersistence(PersistenceManagerFactory pmf) {
-		return pmf.getPersistenceManager();
-		
 	}
 
 }
