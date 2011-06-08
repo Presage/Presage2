8a919fa78b92c0bbe10fae31ab148b4a5d12bdd0
Sam Macbeth <sm1106@imperial.ac.uk>
2011-06-08 19:11:31 +0100

    EnvironmentMembersServices - low level EnvironmentService for getting the simulation members' UUIDs.

---
+++ core/src/main/java/uk/ac/imperial/presage2/core/environment/EnvironmentMembersService.java
@@ -0,0 +1,57 @@
+/**
+ * 	Copyright (C) 2011 Sam Macbeth <sm1106 [at] imperial [dot] ac [dot] uk>
+ *
+ * 	This file is part of Presage2.
+ *
+ *     Presage2 is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Presage2 is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser Public License
+ *     along with Presage2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.imperial.presage2.core.environment;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.UUID;
+
+
+/**
+ * <p>This service provides access to the UUIDs of all the participants in the
+ * simulation. This is a low level service to allow other services to do searches
+ * on participants' shared states.</p>
+ * 
+ * 
+ * @author Sam Macbeth
+ *
+ */
+public class EnvironmentMembersService extends EnvironmentService {
+
+	/**
+	 * @param sharedState
+	 */
+	public EnvironmentMembersService(EnvironmentSharedStateAccess sharedState) {
+		super(sharedState);
+	}
+
+	/**
+	 * Get a {@link Set} of the {@link UUID}s of the participants in the environment.
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public Set<UUID> getParticipants() {
+		try {
+			return Collections.unmodifiableSet((Set<UUID>) sharedState.getGlobal("participants").getValue());
+		} catch(ClassCastException e) {
+			throw e;
+		}
+	}
+
+}
