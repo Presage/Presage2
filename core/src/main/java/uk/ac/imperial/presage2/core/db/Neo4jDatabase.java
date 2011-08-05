87adf2d93993d3cadcf92c8d9d2de0436ffdb1dd
Sam Macbeth <sm1106@imperial.ac.uk>
2011-08-05 14:47:43 +0100

    License headers.

--- core/src/main/java/uk/ac/imperial/presage2/core/db/Neo4jDatabase.java
+++ core/src/main/java/uk/ac/imperial/presage2/core/db/Neo4jDatabase.java
@@ -1,3 +1,21 @@
+/**
+ * 	Copyright (C) 2011 Sam Macbeth <sm1106 [at] imperial [dot] ac [dot] uk>
+ *
+ * 	This file is part of Presage2.
+ *
+ *     Presage2 is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Presage2 is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser Public License
+ *     along with Presage2.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package uk.ac.imperial.presage2.core.db;
 
 import org.apache.log4j.Logger;
@@ -25,6 +43,7 @@ public class Neo4jDatabase implements DatabaseService, GraphDB {
 		if (graphDB == null) {
 			logger.info("Starting embedded Neo4j database at " + databasePath);
 			graphDB = new EmbeddedGraphDatabase(databasePath);
+			//graphDB = new RestGraphDatabase(new URI("http://sam-desktop.local:7474/db/data"));
 			
 			// check/create initial graph structure
 			Transaction tx = graphDB.beginTx();
