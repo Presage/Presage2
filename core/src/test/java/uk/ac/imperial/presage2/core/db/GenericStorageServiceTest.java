5b4cbc46a01968b2ba01820251c296b2cdc3ddd2
Sam Macbeth <sm1106@imperial.ac.uk>
2011-10-21 16:37:37 +0100

    Moved GenericStorageServiceTest into core to allow db modules to depend on it and building test-jar.

--- core/src/test/java/uk/ac/imperial/presage2/core/db/GenericStorageServiceTest.java
+++ core/src/test/java/uk/ac/imperial/presage2/core/db/GenericStorageServiceTest.java
@@ -1,4 +1,22 @@
-package uk.ac.imperial.presage2.db;
+/**
+ * 	Copyright (C) 2011 Sam Macbeth <sm1106 [at] imperial [dot] ac [dot] uk>
+ *
+ * 	This file is part of Presage2.
+ *
+ *     Presage2 is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Presage2 is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser Public License
+ *     along with Presage2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.imperial.presage2.core.db;
 
 import static org.junit.Assert.*;
 
@@ -6,8 +24,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import uk.ac.imperial.presage2.core.db.DatabaseService;
-import uk.ac.imperial.presage2.core.db.StorageService;
 import uk.ac.imperial.presage2.core.db.persistent.PersistentSimulation;
 import uk.ac.imperial.presage2.core.util.random.Random;
 
