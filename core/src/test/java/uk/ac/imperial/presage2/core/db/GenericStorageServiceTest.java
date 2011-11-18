4e44dbcb120e374361af7c5a3d15b83604c2a03f
Sam Macbeth <sm1106@imperial.ac.uk>
2011-11-18 17:26:21 +0000

    Expanded db test to coverage of the majority of the storage api.

--- core/src/test/java/uk/ac/imperial/presage2/core/db/GenericStorageServiceTest.java
+++ core/src/test/java/uk/ac/imperial/presage2/core/db/GenericStorageServiceTest.java
@@ -20,11 +20,17 @@ package uk.ac.imperial.presage2.core.db;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import uk.ac.imperial.presage2.core.db.persistent.PersistentAgent;
 import uk.ac.imperial.presage2.core.db.persistent.PersistentSimulation;
+import uk.ac.imperial.presage2.core.db.persistent.TransientAgentState;
 import uk.ac.imperial.presage2.core.util.random.Random;
 
 public abstract class GenericStorageServiceTest {
@@ -50,10 +56,10 @@ public abstract class GenericStorageServiceTest {
 	public abstract void getDatabase();
 
 	@Test
-	public void testCreateSimulation() {
-		final String simName = "Test";
-		final String simClass = "Testclass";
-		final String simState = "TEST";
+	public void testStorage() {
+		final String simName = RandomStringUtils.randomAlphanumeric(Random.randomInt(20));
+		final String simClass = RandomStringUtils.randomAlphanumeric(Random.randomInt(100));
+		final String simState = RandomStringUtils.randomAlphanumeric(Random.randomInt(80));
 		final int simFinish = Random.randomInt(100);
 		final long timeBefore = System.currentTimeMillis();
 
@@ -76,6 +82,138 @@ public abstract class GenericStorageServiceTest {
 		assertEquals(0, sim.getStartedAt());
 		assertEquals(simState, sim.getState());
 
+		// check getSimulation() returns the current sim
+		assertEquals(simID, sto.getSimulation().getID());
+
+		// check getSimulationByID() returns current sim
+		assertEquals(simID, sto.getSimulationById(simID).getID());
+
+		// check getSimulationbyId() for non existing sim returns null
+		assertNull(sto.getSimulationById(1875063));
+
+		// setSimulation()
+		sto.setSimulation(null);
+		assertNull(sto.getSimulation());
+		sto.setSimulation(sim);
+		assertEquals(simID, sto.getSimulation().getID());
+
+		// create agents
+		final UUID aid = Random.randomUUID();
+		final String agName = RandomStringUtils.randomAlphanumeric(Random.randomInt(20));
+		final PersistentAgent agent = sto.createAgent(aid, agName);
+
+		// assert state of agent
+		assertEquals(aid, agent.getID());
+		assertEquals(agName, agent.getName());
+		// check simulation now has an agent
+		assertFalse(sim.getAgents().isEmpty());
+		assertTrue(sim.getAgents().size() == 1);
+
+		// check transient agent state
+		final int stateTime = Random.randomInt(100);
+		final TransientAgentState state = sto.getAgentState(aid, stateTime);
+		assertNotNull(state);
+		assertEquals(aid, state.getAgent().getID());
+		assertEquals(stateTime, state.getTime());
+
+	}
+
+	@Test
+	public void testSimulation() {
+		// create a simulation
+		final String simName = RandomStringUtils.randomAlphanumeric(Random.randomInt(20));
+		final String simClass = RandomStringUtils.randomAlphanumeric(Random.randomInt(100));
+		final String simState = RandomStringUtils.randomAlphanumeric(Random.randomInt(80));
+		final int simFinish = Random.randomInt(100);
+
+		final PersistentSimulation sim = sto.createSimulation(simName, simClass, simState,
+				simFinish);
+		final long simID = sim.getID();
+
+		// test setters
+		final int newTime = Random.randomInt(Integer.MAX_VALUE);
+		sim.setCurrentTime(newTime);
+		assertEquals(newTime, sim.getCurrentTime());
+		final long newFinish = Random.getInstance().nextLong();
+		sim.setFinishedAt(newFinish);
+		assertEquals(newFinish, sim.getFinishedAt());
+		final long newStart = Random.getInstance().nextLong();
+		sim.setStartedAt(newStart);
+		assertEquals(newStart, sim.getStartedAt());
+		final String newState = RandomStringUtils.randomAlphanumeric(Random.randomInt(80));
+		sim.setState(newState);
+		assertEquals(newState, sim.getState());
+
+		// get a new copy of sim from db to check values were persisted
+		final PersistentSimulation sim2 = sto.getSimulationById(simID);
+		assertEquals(newTime, sim2.getCurrentTime());
+		assertEquals(newFinish, sim2.getFinishedAt());
+		assertEquals(newStart, sim2.getStartedAt());
+		assertEquals(newState, sim2.getState());
+
+		// add parameter
+		final String paramName1 = RandomStringUtils.randomAlphanumeric(Random.randomInt(20));
+		final String paramValue1 = RandomStringUtils.randomAlphanumeric(Random.randomInt(200));
+		sim.addParameter(paramName1, paramValue1);
+		Map<String, Object> params = sim.getParameters();
+		assertTrue(params.size() == 1);
+		assertTrue(params.containsKey(paramName1));
+		assertEquals(paramValue1, params.get(paramName1).toString());
+
+		final String paramName2 = RandomStringUtils.randomAlphanumeric(Random.randomInt(20));
+		final String paramValue2 = RandomStringUtils.randomAlphanumeric(Random.randomInt(200));
+		sim.addParameter(paramName2, paramValue2);
+		params = sim.getParameters();
+		assertTrue(params.size() == 2);
+		assertTrue(params.containsKey(paramName2));
+		assertEquals(paramValue2, params.get(paramName2).toString());
+	}
+
+	@Test
+	public void testAgent() {
+		PersistentAgent agent;
+		try {
+			agent = sto.createAgent(Random.randomUUID(), RandomStringUtils.randomAlphanumeric(10));
+			fail();
+		} catch (RuntimeException e) {
+		}
+		sto.createSimulation(RandomStringUtils.randomAlphanumeric(Random.randomInt(20)),
+				RandomStringUtils.randomAlphanumeric(Random.randomInt(100)),
+				RandomStringUtils.randomAlphanumeric(Random.randomInt(80)), Random.randomInt(100));
+
+		agent = sto.createAgent(Random.randomUUID(), RandomStringUtils.randomAlphanumeric(10));
+
+		// test agent properties
+		assertNull(agent.getProperty(RandomStringUtils.randomAlphabetic(Random.randomInt(20))));
+
+		final String propKey = RandomStringUtils.randomAlphanumeric(Random.randomInt(20));
+		final String propVal = RandomStringUtils.randomAlphanumeric(Random.randomInt(200));
+		agent.setProperty(propKey, propVal);
+		assertNull(agent.getProperty(RandomStringUtils.randomAlphabetic(Random.randomInt(20))));
+		assertEquals(propVal, agent.getProperty(propKey).toString());
+
+		// test agent state
+		final TransientAgentState state = agent.getState(0);
+		assertEquals(agent.getID(), state.getAgent().getID());
+		assertEquals(0, state.getTime());
+
+		assertNull(state.getProperty(RandomStringUtils.randomAlphabetic(Random.randomInt(20))));
+
+		// set and get a property on the state
+		final String spropKey = RandomStringUtils.randomAlphanumeric(Random.randomInt(20));
+		final String spropVal = RandomStringUtils.randomAlphanumeric(Random.randomInt(200));
+		state.setProperty(spropKey, spropVal);
+		assertEquals(spropVal, state.getProperty(spropKey).toString());
+		assertNull(state.getProperty(RandomStringUtils.randomAlphabetic(Random.randomInt(20))));
+
+		// get same property from state for different time
+		final TransientAgentState state2 = agent.getState(Random.randomInt(1000) + 1);
+		assertNull(state2.getProperty(spropKey));
+
+		// check our data has persisted
+		final PersistentAgent ag = sto.getAgent(agent.getID());
+		assertEquals(propVal, ag.getProperty(propKey).toString());
+		assertEquals(spropVal, ag.getState(0).getProperty(spropKey).toString());
 	}
 
 }
