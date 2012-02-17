406748b7433d90f97ec6d450fd5190c365db7ab8
Sam Macbeth <sm1106@imperial.ac.uk>
2012-02-17 22:08:38 +0000

    Extension of AbstractEnvironmentModule to provide bindings for environment services provided to participants on registration. Supports method chaining to allow more concise bindings.

--- util/src/main/java/uk/ac/imperial/presage2/util/environment/AbstractEnvironmentModule.java
+++ util/src/main/java/uk/ac/imperial/presage2/util/environment/AbstractEnvironmentModule.java
@@ -28,16 +28,21 @@ import uk.ac.imperial.presage2.core.environment.EnvironmentServiceProvider;
 import uk.ac.imperial.presage2.core.environment.EnvironmentSharedStateAccess;
 import uk.ac.imperial.presage2.core.environment.ServiceDependencies;
 import uk.ac.imperial.presage2.core.environment.SharedStateStorage;
+import uk.ac.imperial.presage2.core.participant.Participant;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
 import com.google.inject.multibindings.Multibinder;
+import com.google.inject.name.Names;
 
 public class AbstractEnvironmentModule extends AbstractModule {
 
 	final Class<? extends AbstractEnvironment> environmentImplementation;
 
 	final Set<Class<? extends EnvironmentService>> environmentServices;
+	final Set<Class<? extends EnvironmentService>> participantEnvironmentServices;
+	final Set<Class<? extends EnvironmentService>> participantGlobalEnvironmentServices;
 
 	final Set<Class<? extends ActionHandler>> actionHandlers;
 
@@ -61,7 +66,143 @@ public class AbstractEnvironmentModule extends AbstractModule {
 		this.environmentImplementation = environmentImplementation;
 		this.environmentServices = environmentServices;
 		this.actionHandlers = actionHandlers;
-		this.processServiceDependencies();
+		this.participantEnvironmentServices = new HashSet<Class<? extends EnvironmentService>>();
+		this.participantGlobalEnvironmentServices = new HashSet<Class<? extends EnvironmentService>>();
+	}
+
+	/**
+	 * <p>
+	 * Module to bind components required for an {@link AbstractEnvironment}.
+	 * </p>
+	 * 
+	 * @param environmentImplementation
+	 *            Environment implementation to use.
+	 */
+	public AbstractEnvironmentModule(
+			Class<? extends AbstractEnvironment> environmentImplementation) {
+		this(environmentImplementation,
+				new HashSet<Class<? extends EnvironmentService>>(),
+				new HashSet<Class<? extends ActionHandler>>());
+	}
+
+	/**
+	 * Add global environment services to be bound to the environment's service
+	 * provider.
+	 * 
+	 * @param services
+	 *            {@link EnvironmentService} classes to add
+	 * @return this
+	 */
+	public AbstractEnvironmentModule addGlobalEnvironmentServices(
+			Class<? extends EnvironmentService>... services) {
+		for (Class<? extends EnvironmentService> s : services) {
+			this.environmentServices.add(s);
+		}
+		return this;
+	}
+
+	/**
+	 * Add a global environment service to be bound to the environment's service
+	 * provider.
+	 * 
+	 * @param service
+	 *            {@link EnvironmentService} class to add
+	 * @return this
+	 */
+	public AbstractEnvironmentModule addGlobalEnvironmentService(
+			Class<? extends EnvironmentService> service) {
+		this.environmentServices.add(service);
+		return this;
+	}
+
+	/**
+	 * Add {@link ActionHandler}s to be bound to the environment.
+	 * 
+	 * @param handlers
+	 *            {@link ActionHandler}s to bind.
+	 * @return this
+	 */
+	public AbstractEnvironmentModule addActionHandlers(
+			Class<? extends ActionHandler>... handlers) {
+		for (Class<? extends ActionHandler> h : handlers) {
+			this.actionHandlers.add(h);
+		}
+		return this;
+	}
+
+	/**
+	 * Add an {@link ActionHandler} to be bound to the environment.
+	 * 
+	 * @param handler
+	 *            {@link ActionHandler} to bind.
+	 * @return this.
+	 */
+	public AbstractEnvironmentModule addActionHandler(
+			Class<? extends ActionHandler> handler) {
+		this.actionHandlers.add(handler);
+		return this;
+	}
+
+	/**
+	 * Add {@link EnvironmentService}s which will be provided to
+	 * {@link Participant}s on registration with the environment.
+	 * 
+	 * @param services
+	 *            {@link EnvironmentService}s to add.
+	 * @return this
+	 */
+	public AbstractEnvironmentModule addParticipantEnvironmentServices(
+			Class<? extends EnvironmentService>... services) {
+		for (Class<? extends EnvironmentService> s : services) {
+			this.participantEnvironmentServices.add(s);
+		}
+		return this;
+	}
+
+	/**
+	 * Add an {@link EnvironmentService} which will be provided to
+	 * {@link Participant}s on registration with the environment.
+	 * 
+	 * @param services
+	 *            {@link EnvironmentService} to add.
+	 * @return this
+	 */
+	public AbstractEnvironmentModule addParticipantEnvironmentService(
+			Class<? extends EnvironmentService> service) {
+		this.participantEnvironmentServices.add(service);
+		return this;
+	}
+
+	/**
+	 * Add global {@link EnvironmentService}s which will be provided to
+	 * {@link Participant}s on registration with the environment.
+	 * 
+	 * @param services
+	 *            {@link EnvironmentService}s to add.
+	 * @return this
+	 */
+	public AbstractEnvironmentModule addParticipantGlobalEnvironmentServices(
+			Class<? extends EnvironmentService>... services) {
+		for (Class<? extends EnvironmentService> s : services) {
+			this.environmentServices.add(s);
+			this.participantGlobalEnvironmentServices.add(s);
+		}
+		return this;
+	}
+
+	/**
+	 * Add a global {@link EnvironmentService} which will be provided to
+	 * {@link Participant}s on registration with the environment.
+	 * 
+	 * @param services
+	 *            {@link EnvironmentService} to add.
+	 * @return this
+	 */
+	public AbstractEnvironmentModule addParticipantGlobalEnvironmentService(
+			Class<? extends EnvironmentService> service) {
+		this.environmentServices.add(service);
+		this.participantGlobalEnvironmentServices.add(service);
+		return this;
 	}
 
 	/**
@@ -73,16 +214,16 @@ public class AbstractEnvironmentModule extends AbstractModule {
 		final Set<Class<? extends EnvironmentService>> dependencies = new HashSet<Class<? extends EnvironmentService>>();
 		for (Class<? extends EnvironmentService> service : environmentServices) {
 			if (service.isAnnotationPresent(ServiceDependencies.class)) {
-				for (Class<? extends EnvironmentService> dep : service.getAnnotation(
-						ServiceDependencies.class).value()) {
+				for (Class<? extends EnvironmentService> dep : service
+						.getAnnotation(ServiceDependencies.class).value()) {
 					dependencies.add(dep);
 				}
 			}
 		}
 		for (Class<? extends ActionHandler> handler : actionHandlers) {
 			if (handler.isAnnotationPresent(ServiceDependencies.class)) {
-				for (Class<? extends EnvironmentService> dep : handler.getAnnotation(
-						ServiceDependencies.class).value()) {
+				for (Class<? extends EnvironmentService> dep : handler
+						.getAnnotation(ServiceDependencies.class).value()) {
 					dependencies.add(dep);
 				}
 			}
@@ -92,6 +233,9 @@ public class AbstractEnvironmentModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
+		// resolve any additional service dependencies
+		this.processServiceDependencies();
+
 		// bind AbstractEnvironment interfaces
 		bind(EnvironmentConnector.class).to(AbstractEnvironment.class);
 		bind(EnvironmentServiceProvider.class).to(AbstractEnvironment.class);
@@ -100,21 +244,39 @@ public class AbstractEnvironmentModule extends AbstractModule {
 		bind(MappedSharedState.class).in(Singleton.class);
 
 		// bind Singleton implementation to AbstractEnvironment
-		bind(AbstractEnvironment.class).to(environmentImplementation).in(Singleton.class);
+		bind(AbstractEnvironment.class).to(environmentImplementation).in(
+				Singleton.class);
 
 		// global environment services
-		Multibinder<EnvironmentService> serviceBinder = Multibinder.newSetBinder(binder(),
-				EnvironmentService.class);
+		Multibinder<EnvironmentService> serviceBinder = Multibinder
+				.newSetBinder(binder(), EnvironmentService.class);
 		for (Class<? extends EnvironmentService> service : environmentServices) {
-
 			serviceBinder.addBinding().to(service);
 		}
 
 		// action handlers
-		Multibinder<ActionHandler> actionBinder = Multibinder.newSetBinder(binder(),
-				ActionHandler.class);
+		Multibinder<ActionHandler> actionBinder = Multibinder.newSetBinder(
+				binder(), ActionHandler.class);
 		for (Class<? extends ActionHandler> handler : actionHandlers) {
 			actionBinder.addBinding().to(handler);
+		}
+
+		// participant environment services
+		Multibinder<Class<? extends EnvironmentService>> participantServiceBinder = Multibinder
+				.newSetBinder(binder(),
+						new TypeLiteral<Class<? extends EnvironmentService>>() {
+						}, Names.named("participantEnvironmentServices"));
+		for (Class<? extends EnvironmentService> service : participantEnvironmentServices) {
+			participantServiceBinder.addBinding().toInstance(service);
+		}
+
+		// participant global environment services
+		Multibinder<Class<? extends EnvironmentService>> participantGlobalServiceBinder = Multibinder
+				.newSetBinder(binder(),
+						new TypeLiteral<Class<? extends EnvironmentService>>() {
+						}, Names.named("participantGlobalEnvironmentServices"));
+		for (Class<? extends EnvironmentService> service : participantGlobalEnvironmentServices) {
+			participantGlobalServiceBinder.addBinding().toInstance(service);
 		}
 	}
 
