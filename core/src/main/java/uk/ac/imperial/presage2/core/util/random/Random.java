b6cd0a6d47acc00bcacb0a680f3531e22af5f17e
Sam Macbeth <sm1106@imperial.ac.uk>
2012-05-17 16:29:28 +0100

    Allow custom definition of random seed.

--- core/src/main/java/uk/ac/imperial/presage2/core/util/random/Random.java
+++ core/src/main/java/uk/ac/imperial/presage2/core/util/random/Random.java
@@ -24,70 +24,76 @@ import java.util.UUID;
 import com.google.inject.Singleton;
 
 /**
- * This is a wrapper for {@link java.util.Random} to provide static
- * access to most of it's methods via a singleton, and to control the initial
- * random seed.
+ * This is a wrapper for {@link java.util.Random} to provide static access to
+ * most of it's methods via a singleton, and to control the initial random seed.
  * 
  * 
  * @author Sam Macbeth
- *
+ * 
  */
 @Singleton
 public class Random extends java.util.Random {
 
+	public static long seed = 0;
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private static Random INSTANCE = null;
-	
+
 	protected Random(long seed) {
 		super(seed);
 	}
-	
+
 	protected Random() {
 		super();
 	}
-	
+
 	/**
 	 * Get the instance of {@link Random}.
+	 * 
 	 * @return
 	 */
 	public static synchronized Random getInstance() {
-		if(INSTANCE == null) {
+		if (INSTANCE == null) {
 			INSTANCE = RandomFactory.create();
 		}
 		return INSTANCE;
 	}
-	
+
 	/**
 	 * Wrapper for {@link UUID#randomUUID()}
+	 * 
 	 * @return
 	 */
 	public static UUID randomUUID() {
 		return UUID.randomUUID();
 	}
-	
+
 	/**
 	 * Wrapper for {@link java.util.Random#nextInt()}
+	 * 
 	 * @return
 	 */
 	public static int randomInt() {
 		return Random.getInstance().nextInt();
 	}
-	
+
 	/**
 	 * Wrapper for {@link java.util.Random#nextInt(int)}
+	 * 
 	 * @return
 	 */
 	public static int randomInt(int n) {
 		return Random.getInstance().nextInt(n);
 	}
-	
+
 	/**
 	 * Wrapper for {@link java.util.Random#nextDouble()}
+	 * 
 	 * @return
 	 */
 	public static double randomDouble() {
 		return Random.getInstance().nextDouble();
 	}
-	
+
 }
--- core/src/main/java/uk/ac/imperial/presage2/core/util/random/RandomFactory.java
+++ core/src/main/java/uk/ac/imperial/presage2/core/util/random/RandomFactory.java
@@ -23,19 +23,26 @@ package uk.ac.imperial.presage2.core.util.random;
  * Factory Constructor for {@link Random}.
  * 
  * @author Sam Macbeth
- *
+ * 
  */
 class RandomFactory {
 
 	/**
-	 * <p>Creates an instance of {@link Random}</p>
-	 * <p>Currently simply uses the default constructor, in future we will
-	 * allow it to pull a previously specified random seed to initialise the 
-	 * Random with</p>
+	 * <p>
+	 * Creates an instance of {@link Random}
+	 * </p>
+	 * <p>
+	 * Currently simply uses the default constructor, in future we will allow it
+	 * to pull a previously specified random seed to initialise the Random with
+	 * </p>
+	 * 
 	 * @return
 	 */
 	protected static Random create() {
-		return new Random();
+		if (Random.seed > 0)
+			return new Random(Random.seed);
+		else
+			return new Random();
 	}
-	
+
 }
