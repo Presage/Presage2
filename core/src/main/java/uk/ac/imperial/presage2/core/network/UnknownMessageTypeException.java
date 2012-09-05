327b2eb3b4cf24f1d776954c389bb6b7b58f2b53
Sam Macbeth <sm1106@imperial.ac.uk>
2012-09-05 13:59:31 +0100

    Remove generics from Message in network exceptions.

--- core/src/main/java/uk/ac/imperial/presage2/core/network/UnknownMessageTypeException.java
+++ core/src/main/java/uk/ac/imperial/presage2/core/network/UnknownMessageTypeException.java
@@ -30,12 +30,12 @@ public class UnknownMessageTypeException extends NetworkException {
 	 */
 	private static final long serialVersionUID = 6075812255856879414L;
 
-	final protected Message<?> message;
+	final protected Message message;
 
 	/**
 	 * @param message
 	 */
-	public UnknownMessageTypeException(Message<?> message) {
+	public UnknownMessageTypeException(Message message) {
 		super("Unknown message: " + message.toString());
 		this.message = message;
 	}
