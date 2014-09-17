e00ba38367879b67d730498d2c569c91a03d006f
Sam Macbeth <sm1106@imperial.ac.uk>
2014-09-17 18:33:28 +0100

    Rewrite for new storage API and using TupleStorageService.

--- src/main/java/uk/ac/imperial/presage2/db/json/JsonStorage.java
+++ src/main/java/uk/ac/imperial/presage2/db/json/JsonStorage.java
@@ -1,5 +1,5 @@
 /**
- * 	Copyright (C) 2011-2012 Sam Macbeth <sm1106 [at] imperial [dot] ac [dot] uk>
+ * 	Copyright (C) 2011-2014 Sam Macbeth <sm1106 [at] imperial [dot] ac [dot] uk>
  *
  * 	This file is part of Presage2.
  *
@@ -20,36 +20,34 @@ package uk.ac.imperial.presage2.db.json;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.UUID;
 
 import org.apache.log4j.Logger;
 
 import uk.ac.imperial.presage2.core.db.DatabaseService;
-import uk.ac.imperial.presage2.core.db.StorageService;
-import uk.ac.imperial.presage2.core.db.persistent.PersistentAgent;
-import uk.ac.imperial.presage2.core.db.persistent.PersistentEnvironment;
-import uk.ac.imperial.presage2.core.db.persistent.PersistentSimulation;
-import uk.ac.imperial.presage2.core.db.persistent.TransientAgentState;
+import uk.ac.imperial.presage2.core.db.TupleStorageService;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Inject;
-import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 
-@Singleton
-public class JsonStorage implements StorageService, DatabaseService {
+public class JsonStorage extends TupleStorageService implements
+		DatabaseService {
 
 	private final Logger logger = Logger.getLogger(JsonStorage.class);
 
@@ -59,23 +57,27 @@ public class JsonStorage implements StorageService, DatabaseService {
 	boolean started = false;
 	long maxSimId = 0;
 
-	List<SimulationBean> simulations;
-	SimulationBean currentSimulation = null;
-	long agentsLoaded = 0;
-	Map<UUID, PersistentAgent> agents = new HashMap<UUID, PersistentAgent>();
-
 	ObjectMapper mapper;
-	Set<String> writeQueue = new HashSet<String>();
-	Map<String, Object> jsonObjects = new HashMap<String, Object>();
+	ArrayNode root;
+
+	Map<Long, ObjectNode> simulations;
 
 	public JsonStorage() {
 		this("data/");
 	}
 
 	@Inject
-	public JsonStorage(@Named(JsonModule.STORAGEPATH_KEY) String baseStoragePath) {
+	public JsonStorage(
+			@Named(JsonModule.STORAGEPATH_KEY) String baseStoragePath) {
 		super();
 		this.baseStoragePath = baseStoragePath;
+		mapper = new ObjectMapper();
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+	}
+
+	@Override
+	public void start() throws Exception {
+		// check base path
 		if (this.baseStoragePath.length() > 0) {
 			File basePath = new File(this.baseStoragePath);
 			if (!basePath.exists()) {
@@ -85,36 +87,35 @@ public class JsonStorage implements StorageService, DatabaseService {
 				throw new RuntimeException("Base path is not a directory.");
 			}
 		}
-
-		mapper = new ObjectMapper();
-		mapper.enable(SerializationFeature.INDENT_OUTPUT);
-	}
-
-	@Override
-	public void start() throws Exception {
 		// open simulations file if exists, create otherwise
 		File simulationsFile = new File(baseStoragePath + simulationsFileName);
 
 		if (!simulationsFile.exists()) {
 			simulationsFile.createNewFile();
-			ArrayNode n = mapper.createArrayNode();
-			mapper.writeValue(simulationsFile, n);
+			root = mapper.createArrayNode();
+			mapper.writeValue(simulationsFile, root);
 		}
 
 		// read simulations content in to JsonNode
+		simulations = new HashMap<Long, ObjectNode>();
 		try {
-			simulations = mapper.readValue(simulationsFile,
-					new TypeReference<List<SimulationBean>>() {
-					});
+			root = mapper.readValue(simulationsFile, ArrayNode.class);
+
+			for (JsonNode s : root) {
+				simulations.put(s.get("id").asLong(), (ObjectNode) s);
+			}
 		} catch (JsonMappingException e) {
-			simulations = new LinkedList<JsonStorage.SimulationBean>();
 			logger.warn("Couldn't parse simulations file: " + baseStoragePath
 					+ simulationsFileName, e);
 		}
 		// find max simID for new insertions
-		for (SimulationBean s : simulations) {
-			maxSimId = Math.max(maxSimId, s.id);
+		try {
+			maxSimId = Collections.max(simulations.keySet());
+		} catch (NoSuchElementException e) {
+			// empty set
+			maxSimId = 0;
 		}
+
 		started = true;
 	}
 
@@ -130,7 +131,7 @@ public class JsonStorage implements StorageService, DatabaseService {
 			File simulationsFile = new File(baseStoragePath
 					+ simulationsFileName);
 			try {
-				mapper.writeValue(simulationsFile, simulations);
+				mapper.writeValue(simulationsFile, root);
 			} catch (JsonGenerationException e) {
 				e.printStackTrace();
 			} catch (JsonMappingException e) {
@@ -138,479 +139,164 @@ public class JsonStorage implements StorageService, DatabaseService {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
-			for (String fileName : writeQueue) {
-				if (!jsonObjects.containsKey(fileName)) {
-					logger.warn("Missing data for " + fileName);
-					continue;
-				}
-				try {
-					File f = new File(fileName);
-					if (!f.exists()) {
-						f.getParentFile().mkdirs();
-						f.createNewFile();
-					}
-					mapper.writeValue(f, jsonObjects.get(fileName));
-				} catch (IOException e) {
-					logger.warn("Error writing file " + fileName, e);
-				}
-			}
 		}
-
 		started = false;
 	}
 
 	@Override
-	public PersistentSimulation createSimulation(String name, String classname,
-			String state, int finishTime) {
-		this.currentSimulation = new SimulationBean(++maxSimId, name,
-				classname, state, finishTime);
-		this.simulations.add(currentSimulation);
-		return new Simulation(currentSimulation);
-	}
-
-	@Override
-	public PersistentSimulation getSimulation() {
-		if (currentSimulation != null)
-			return new Simulation(currentSimulation);
-		return null;
-	}
-
-	@Override
-	public PersistentSimulation getSimulationById(long id) {
-		SimulationBean s = findSimulationId(id);
-		if (s != null)
-			return new Simulation(s);
-		else
-			return null;
-	}
-
-	@Override
 	public List<Long> getSimulations() {
-		List<Long> ids = new LinkedList<Long>();
-		for (SimulationBean sim : simulations) {
-			ids.add(sim.id);
-		}
-		return ids;
+		return Collections.unmodifiableList(new ArrayList<Long>(simulations
+				.keySet()));
 	}
 
 	@Override
-	public void setSimulation(PersistentSimulation sim) {
-		if (sim == null)
-			this.currentSimulation = null;
-		else
-			this.currentSimulation = findSimulationId(sim.getID());
+	protected long getNextId() {
+		return ++maxSimId;
+	}
+
+	private ObjectNode getSim(long id) {
+		ObjectNode s = simulations.get(id);
+		if (s == null) {
+			s = mapper.createObjectNode();
+			s.put("id", id);
+			simulations.put(id, s);
+			root.add(s);
+		}
+		return s;
+	}
+
+	private <T> T returnAsType(JsonNode n, Class<T> type) {
+		if (type == String.class)
+			return type.cast(n.textValue());
+		else if (type == Integer.class || type == Integer.TYPE)
+			return type.cast(n.asInt());
+		else if (type == Double.class || type == Double.TYPE)
+			return type.cast(n.asDouble());
+		else if (type == Boolean.class || type == Boolean.TYPE)
+			return type.cast(n.asBoolean());
+		else if (type == Long.class || type == Long.TYPE)
+			return type.cast(n.asLong());
+
+		throw new RuntimeException("Unknown type cast request");
 	}
 
 	@Override
-	public PersistentAgent createAgent(UUID agentID, String name) {
-		if (this.currentSimulation != null) {
-			loadAgents(this.currentSimulation.id);
-
-			Agent agent = new Agent();
-			agent.iD = agentID;
-			agent.name = name;
-
-			this.currentSimulation.agents.add(agent);
-			this.agents.put(agentID, agent);
-
-			return agent;
-		}
-		throw new RuntimeException("Cannot create agent with no simulation.");
+	protected void storeParameter(long id, String key, String value) {
+		getSim(id).with("parameters").put(key, value);
 	}
 
 	@Override
-	public PersistentAgent getAgent(UUID agentID) {
-		if (this.currentSimulation != null) {
-			loadAgents(this.currentSimulation.id);
-
-			return this.agents.get(agentID);
-		}
-		return null;
+	protected void storeTuple(long id, String key, String value) {
+		getSim(id).with("properties").put(key, value);
 	}
 
 	@Override
-	public TransientAgentState getAgentState(UUID agentID, int time) {
-		try {
-			return getAgent(agentID).getState(time);
-		} catch (NullPointerException e) {
-			return null;
-		}
+	protected void storeTuple(long id, String key, int value) {
+		getSim(id).with("properties").put(key, value);
 	}
 
-	private SimulationBean findSimulationId(long id) {
-		for (SimulationBean sim : simulations) {
-			if (sim.id == id)
-				return sim;
-		}
-		return null;
+	@Override
+	protected void storeTuple(long id, String key, double value) {
+		getSim(id).with("properties").put(key, value);
 	}
 
-	private void addJsonObject(String filename, Object object) {
-		this.writeQueue.add(filename);
-		this.jsonObjects.put(filename, object);
+	@Override
+	protected void storeTuple(long id, String key, int t, String value) {
+		getSim(id).with("transproperties").with(Integer.toString(t))
+				.put(key, value);
 	}
 
-	private PersistentEnvironment getEnvironment(long simId) {
-		Environment env;
-		String filename = baseStoragePath + "env_" + simId + ".json";
-		File envFile = new File(filename);
-		try {
-			if (envFile.exists()) {
-				env = mapper.readValue(envFile, Environment.class);
-			} else {
-				env = new Environment();
-				env.simId = simId;
-			}
-			addJsonObject(filename, env);
-			return env;
-		} catch (IOException e) {
-			logger.warn("Unable to read environment file", e);
-			return null;
-		}
+	@Override
+	protected void storeTuple(long id, String key, int t, int value) {
+		getSim(id).with("transproperties").with(Integer.toString(t))
+				.put(key, value);
 	}
 
-	private void loadAgents(long simId) {
-		if (agentsLoaded != simId) {
-			SimulationBean sim = findSimulationId(simId);
-			if (sim == null)
-				return;
-
-			String filename = baseStoragePath + "agents_" + simId + ".json";
-			File agentsFile = new File(filename);
-			try {
-				// load agent data from json file
-				if (agentsFile.exists()) {
-					sim.agents = mapper.readValue(agentsFile,
-							new TypeReference<List<Agent>>() {
-							});
-				} else {
-					sim.agents = new LinkedList<PersistentAgent>();
-				}
-
-				// store id -> agent index
-				this.agents.clear();
-				for (PersistentAgent ag : sim.agents) {
-					this.agents.put(ag.getID(), ag);
-				}
-				// mark agents list to be written to file.
-				addJsonObject(filename, sim.agents);
-				agentsLoaded = simId;
-			} catch (IOException e) {
-				logger.warn("Unable to read agents file", e);
-			}
-		}
+	@Override
+	protected void storeTuple(long id, String key, int t, double value) {
+		getSim(id).with("transproperties").with(Integer.toString(t))
+				.put(key, value);
 	}
 
-	static class SimulationBean {
-
-		public long id;
-		public String name;
-		public String state;
-		public String className;
-		public int currentTime;
-		public int finishTime;
-		public long createdAt;
-		public long startedAt;
-		public long finishedAt;
-		public Map<String, String> parameters = new HashMap<String, String>();
-		public long parent = 0;
-
-		@JsonIgnore
-		List<PersistentAgent> agents = null;
-
-		public SimulationBean() {
-			super();
-		}
-
-		public SimulationBean(long id, String name, String className,
-				String state, int finishTime) {
-			super();
-			this.id = id;
-			this.name = name;
-			this.className = className;
-			this.currentTime = 0;
-			this.finishTime = finishTime;
-			this.createdAt = System.currentTimeMillis();
-			this.state = state;
-		}
-
+	@Override
+	protected void storeTuple(long id, String key, UUID agent, String value) {
+		getSim(id).with("agentproperties").with(agent.toString())
+				.put(key, value);
 	}
 
-	class Simulation implements PersistentSimulation {
-
-		SimulationBean delegate;
-
-		public Simulation(SimulationBean delegate) {
-			super();
-			this.delegate = delegate;
-		}
-
-		@Override
-		public long getID() {
-			return delegate.id;
-		}
-
-		@Override
-		public void addParameter(String name, String value) {
-			delegate.parameters.put(name, value);
-		}
-
-		@Override
-		public Map<String, String> getParameters() {
-			return delegate.parameters;
-		}
-
-		@Override
-		public int getFinishTime() {
-			return delegate.finishTime;
-		}
-
-		@Override
-		public void setCurrentTime(int time) {
-			delegate.currentTime = time;
-		}
-
-		@Override
-		public int getCurrentTime() {
-			return delegate.currentTime;
-		}
-
-		@Override
-		public void setState(String newState) {
-			delegate.state = newState;
-		}
-
-		@Override
-		public String getState() {
-			return delegate.state;
-		}
-
-		@Override
-		public PersistentEnvironment getEnvironment() {
-			return JsonStorage.this.getEnvironment(getID());
-		}
-
-		@Override
-		public void setParentSimulation(PersistentSimulation parent) {
-			if (parent != null)
-				delegate.parent = parent.getID();
-			else
-				delegate.parent = 0;
-		}
-
-		@Override
-		public PersistentSimulation getParentSimulation() {
-			if (delegate.parent > 0) {
-				SimulationBean s = findSimulationId(delegate.parent);
-				if (s != null)
-					return new Simulation(s);
-			}
-			return null;
-		}
-
-		@Override
-		public List<Long> getChildren() {
-			List<Long> children = new LinkedList<Long>();
-			for (SimulationBean s : simulations) {
-				if (s.parent == getID()) {
-					children.add(s.id);
-				}
-			}
-			return children;
-		}
-
-		@Override
-		public void setFinishedAt(long time) {
-			delegate.finishedAt = time;
-		}
-
-		@Override
-		public long getFinishedAt() {
-			return delegate.finishedAt;
-		}
-
-		@Override
-		public void setStartedAt(long time) {
-			delegate.startedAt = time;
-		}
-
-		@Override
-		public long getStartedAt() {
-			return delegate.startedAt;
-		}
-
-		@Override
-		public long getCreatedAt() {
-			return delegate.createdAt;
-		}
-
-		@Override
-		public String getClassName() {
-			return delegate.className;
-		}
-
-		@Override
-		public String getName() {
-			return delegate.name;
-		}
-
-		@Override
-		public Set<PersistentAgent> getAgents() {
-			loadAgents(getID());
-			return new HashSet<PersistentAgent>(delegate.agents);
-		}
+	@Override
+	protected void storeTuple(long id, String key, UUID agent, int value) {
+		getSim(id).with("agentproperties").with(agent.toString())
+				.put(key, value);
 	}
 
-	class Environment implements PersistentEnvironment {
-
-		public long simId = 0;
-		public Map<String, String> properties = new HashMap<String, String>();
-		public Map<Integer, Map<String, String>> states = new HashMap<Integer, Map<String, String>>();
-
-		public Environment() {
-			super();
-		}
-
-		@Override
-		public Map<String, String> getProperties() {
-			return properties;
-		}
-
-		@Override
-		@JsonIgnore
-		public String getProperty(String key) {
-			return properties.get(key);
-		}
-
-		@Override
-		@JsonIgnore
-		public void setProperty(String key, String value) {
-			properties.put(key, value);
-		}
-
-		@Override
-		@JsonIgnore
-		public Map<String, String> getProperties(int timestep) {
-			if (!states.containsKey(timestep))
-				states.put(timestep, new HashMap<String, String>());
-
-			return states.get(timestep);
-		}
-
-		@Override
-		@JsonIgnore
-		public String getProperty(String key, int timestep) {
-			return getProperties(timestep).get(key);
-		}
-
-		@Override
-		@JsonIgnore
-		public void setProperty(String key, int timestep, String value) {
-			getProperties(timestep).put(key, value);
-		}
-
+	@Override
+	protected void storeTuple(long id, String key, UUID agent, double value) {
+		getSim(id).with("agentproperties").with(agent.toString())
+				.put(key, value);
 	}
 
-	class Agent implements PersistentAgent {
+	@Override
+	protected void storeTuple(long id, String key, UUID agent, int t,
+			String value) {
+		getSim(id).with("agenttransproperties").with(agent.toString())
+				.with(Integer.toString(t)).put(key, value);
+	}
 
-		public UUID iD;
-		public String name;
-		public Map<String, String> properties = new HashMap<String, String>();
-		public Map<Integer, Map<String, String>> state = new HashMap<Integer, Map<String, String>>();
+	@Override
+	protected void storeTuple(long id, String key, UUID agent, int t, int value) {
+		getSim(id).with("agenttransproperties").with(agent.toString())
+				.with(Integer.toString(t)).put(key, value);
+	}
 
-		public Agent() {
-			super();
+	@Override
+	protected void storeTuple(long id, String key, UUID agent, int t,
+			double value) {
+		getSim(id).with("agenttransproperties").with(agent.toString())
+				.with(Integer.toString(t)).put(key, value);
+	}
+
+	@Override
+	protected Set<String> fetchParameterKeys(long id) {
+		Set<String> keys = new HashSet<String>();
+		for (Iterator<String> it = getSim(id).with("parameters").fieldNames(); it
+				.hasNext();) {
+			keys.add(it.next());
 		}
+		return keys;
+	}
 
-		public Agent(UUID iD, String name, Map<String, String> properties) {
-			super();
-			this.iD = iD;
-			this.name = name;
-			this.properties = properties;
-		}
+	@Override
+	protected String fetchParameter(long id, String key) {
+		return getSim(id).with("parameters").path(key).textValue();
+	}
 
-		@Override
-		public UUID getID() {
-			return iD;
-		}
+	@Override
+	protected <T> T fetchTuple(long id, String key, Class<T> type) {
+		JsonNode n = getSim(id).with("properties").path(key);
+		return returnAsType(n, type);
+	}
 
-		@Override
-		public String getName() {
-			return name;
-		}
+	@Override
+	protected <T> T fetchTuple(long id, String key, int t, Class<T> type) {
+		JsonNode n = getSim(id).with("transproperties")
+				.with(Integer.toString(t)).path(key);
+		return returnAsType(n, type);
+	}
 
-		@Override
-		@JsonIgnore
-		public void setRegisteredAt(int time) {
-		}
+	@Override
+	protected <T> T fetchTuple(long id, String key, UUID agent, Class<T> type) {
+		JsonNode n = getSim(id).with("agentproperties").with(agent.toString())
+				.path(key);
+		return returnAsType(n, type);
+	}
 
-		@Override
-		@JsonIgnore
-		public void setDeRegisteredAt(int time) {
-		}
-
-		@Override
-		public Map<String, String> getProperties() {
-			return properties;
-		}
-
-		@Override
-		@JsonIgnore
-		public String getProperty(String key) {
-			return properties.get(key);
-		}
-
-		@Override
-		@JsonIgnore
-		public void setProperty(String key, String value) {
-			properties.put(key, value);
-		}
-
-		@Override
-		@JsonIgnore
-		public TransientAgentState getState(int time) {
-			return new State(time);
-		}
-
-		class State implements TransientAgentState {
-
-			final int time;
-
-			public State(int time) {
-				super();
-				this.time = time;
-				if (!state.containsKey(time))
-					state.put(time, new HashMap<String, String>());
-			}
-
-			@Override
-			public int getTime() {
-				return time;
-			}
-
-			@Override
-			public PersistentAgent getAgent() {
-				return Agent.this;
-			}
-
-			@Override
-			public Map<String, String> getProperties() {
-				return state.get(time);
-			}
-
-			@Override
-			public String getProperty(String key) {
-				return getProperties().get(key);
-			}
-
-			@Override
-			public void setProperty(String key, String value) {
-				getProperties().put(key, value);
-			}
-
-		}
-
+	@Override
+	protected <T> T fetchTuple(long id, String key, UUID agent, int t,
+			Class<T> type) {
+		JsonNode n = getSim(id).with("agenttransproperties")
+				.with(agent.toString()).with(Integer.toString(t)).path(key);
+		return returnAsType(n, type);
 	}
 
 }
